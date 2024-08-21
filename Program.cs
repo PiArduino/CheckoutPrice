d12c2f04919e6f9e9e7f19fed80f234360573c73
PiArduino <36442329+PiArduino@users.noreply.github.com>
2024-08-21 14:31:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+static void main()
+{
+
+
+}
+
+public interface ICheckout
+{
+    void Scan(string item);
+    int GetTotalPrice();
+}
+
+public class Checkout : ICheckout
+{ 
+
+    public void Scan(string item)
+    {
+
+    }
+
+    public int GetTotalPrice()
+    {
+        return 0;
+    }
+}
+
